a097c7f9e53116e567393d2d4d6ec379e49928a5
Andril Omer <andrilomer44@gmail.com>
2026-03-10 22:06:31 +0530

    Generics

---
+++ src/Generics.ts
@@ -0,0 +1,41 @@
+function wrapInArray<T>(item: T): T[] {
+  return [item];
+}
+
+wrapInArray("masala");
+wrapInArray(42);
+wrapInArray({ flavor: "Ginger" });
+
+function pair<A, B>(a: A, b: B): [A, B] {
+  return [a, b];
+}
+
+pair("masala", "test");
+
+interface Box<T> {
+  content: T;
+}
+
+const numberBox: Box<number> = {
+  content: 10,
+};
+
+const numberBoxCup: Box<string> = {
+  content: "andril",
+};
+
+//partial , pick and omit all work with the generics
+
+interface ApiPromise<T> {
+  status: number;
+  data: T;
+}
+
+const res: ApiPromise<{ flavor: string }> = {
+  status: 200,
+  data: {
+    flavor: "masala",
+  },
+};
+
+
